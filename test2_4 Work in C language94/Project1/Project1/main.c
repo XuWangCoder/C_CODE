8d6983b866b61601bbbb9f16de98d085143532c9
XuWangCoder <wx669163@163.com>
2023-02-05 19:34:00 +0800

    2_5

--- test2_4 Work in C language94/Project1/Project1/main.c
+++ test2_4 Work in C language94/Project1/Project1/main.c
@@ -68,23 +68,66 @@
 
 //求两个数二进制中不同位的个数
 //例子：输入：1999 2299   输出：7
-int f1(int a, int b)
+//int f1(int a, int b)
+//{
+//	int c = a ^ b;
+//	int count = 0;
+//	while (c)
+//	{
+//		c = c & (c - 1);
+//		count++;
+//	}
+//}
+//
+//int main() 
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", f1(a, b));
+//	return 0;
+//}
+//
+//int f1(int a , int b)
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((a >> i) & 1) != ((b >> i) & 1))
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+
+
+
+//打印整数二进制的奇数位和偶数位
+int f1(int num)
 {
-	int c = a ^ b;
+	int i = 0;
 	int count = 0;
-	while (c)
+	//打印偶数位
+	for(i = 31; i>=0; i-=2)
 	{
-		c = c & (c - 1);
-		count++;
+		printf("%d ", 1 & (num >> i));
 	}
+	printf("\n");
+	//打印奇数位
+	for (i = 30; i >= 0; i -= 2)
+	{
+		printf("%d ", 1 & (num >> i));
+	}
+
 	
 }
 
 int main()
 {
-	int a = 0;
-	int b = 0;
-	scanf("%d %d", &a, &b);
-	printf("%d", f1(a, b));
+	int num = 0;
+	scanf("%d", &num);
+	f1(num);
 	return 0;
 }
