250b2d55357f95cde5e9f110bf98b3827d2d74fb
XuWangCoder <wx669163@163.com>
2023-02-17 23:13:13 +0800

    2_17

--- test2_15 High order of pointer 125/Project1/Project1/main.c
+++ test2_15 High order of pointer 125/Project1/Project1/main.c
@@ -61,12 +61,36 @@
 
 
 
-int main()
-{
-	int a[4] = { 1,2,3,4 };
-	int* ptr1 = (int*)(&a + 1);
-	int* ptr2 = (int*)((int)a + 1);
+//int main()//?
+//{
+//	int a[4] = { 1,2,3,4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//
+//	printf("%x, %x", ptr1[-1], *ptr2);// 4  2000000
+//	return 0;
+//}
 
-	printf("%x, %x", ptr1[-1], *ptr2);// 4  2000000
-	return 0;
-}
+
+
+//int main()//?
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	//?
+//
+//	return 0;
+//}
+
+
+
+//int main()//?
+//{
+//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
