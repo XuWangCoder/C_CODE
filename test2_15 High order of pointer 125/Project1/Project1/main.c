2d846e1716652a1e82aa0fb341cb89f6a7dff25b
XuWangCoder <wx669163@163.com>
2023-02-18 21:47:25 +0800

    2_18

--- test2_15 High order of pointer 125/Project1/Project1/main.c
+++ test2_15 High order of pointer 125/Project1/Project1/main.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
 //int main()
 //{
 //	int a[3][4] = { 0 };
@@ -32,7 +33,7 @@
 //int main()
 //{
 //	int a[5] = { 1,2,3,4,5 };
-//	int* ptr = (int*)(&a + 1);//(&a+1)指向数组最末端；将指向数组最末端的指针赋给ptr
+//	int* ptr = (int*)(&a + 1);//(&a+1)指向数组最末端；将指向数组最末端的指针赋给ptr    将数组指针int(*)[5] 转换为 整形指针(int*)
 //	printf("%d, %d", *(a + 1), *(ptr - 1));//2  5
 //	return 0;
 //}
@@ -52,19 +53,19 @@
 ////已知，结构体Test类型的变量大小是20个字节
 //int main()
 //{
-//	printf("%p\n", p + 0x1);//0x100014
-//	printf("%p\n", (unsigned long)p + 0x1);//0x100001
+//	printf("%p\n", p + 0x1);//0x100014//十六进制转化为十进制，每一位*10(位数-1 次方)
+//	printf("%p\n", (unsigned long)p + 0x1);//相当于转化成了整形，整形加一，相当于整数加一 0x100001
 //	printf("%p\n", (unsigned int*)p + 0x1);//0x100004
 //	//（int）x是将数据强制转换为int型。而（int *）是强制了指针类型转换
 //	return 0;
 //}
 
 
-
-//int main()//?
+//
+//int main()//
 //{
-//	int a[4] = { 1,2,3,4 };
-//	int* ptr1 = (int*)(&a + 1);
+//	int a ] = { 1,2,3,4 };
+//	int* ptr1 = (int*)(&a + 1);  
 //	int* ptr2 = (int*)((int)a + 1);
 //
 //	printf("%x, %x", ptr1[-1], *ptr2);// 4  2000000
@@ -73,16 +74,27 @@
 
 
 
-//int main()//?
+//int mian()
 //{
-//	int a[5][5];
-//	int(*p)[4];
-//	p = a;
-//	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
-//	//?
-//
+//	int a[3][2] = { (0,1),(2,3),(4,5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d", p[0]);
 //	return 0;
 //}
+
+
+
+int main()//?
+{
+	int a[5][5];
+	int(*p)[4];
+	p = a;
+	printf("%p, %d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+	//?
+
+	return 0;
+}
 
 
 
@@ -92,5 +104,31 @@
 //	int* ptr1 = (int*)(&aa + 1);
 //	int* ptr2 = (int*)(*(aa + 1));
 //	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };//char* p="abcdef" 字符串的首字母地址放在char*p里
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);//at
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp[] = cp;
+//
+//	printf("%s\n", **++cpp);//POINT
+//	printf("%s\n", *-- *++cpp + 3);//ER
+//	printf("%s\n", *cpp[-2] + 3);//ST
+//	printf("%s\n", cpp[-1][-1] + 1);// EW
 //	return 0;
 //}
