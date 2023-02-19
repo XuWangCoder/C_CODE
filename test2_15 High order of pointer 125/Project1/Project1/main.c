3db0c9d4c6f82371d38885800de6d245bb9fe9b4
XuWangCoder <wx669163@163.com>
2023-02-19 21:41:15 +0800

    2_19

--- test2_15 High order of pointer 125/Project1/Project1/main.c
+++ test2_15 High order of pointer 125/Project1/Project1/main.c
@@ -102,7 +102,7 @@ int main()//?
 //{
 //	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
 //	int* ptr1 = (int*)(&aa + 1);
-//	int* ptr2 = (int*)(*(aa + 1));
+//	int* ptr2 = (int*)(*(aa + 1));// *(aa+1)ÊÇ6µÄµØÖ·¼´aa[1]
 //	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
 //	return 0;
 //}
